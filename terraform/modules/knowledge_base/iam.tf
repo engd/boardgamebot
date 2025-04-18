dafd44db52609ca8ef55480530f5785f900b8c56
Engin Dogusoy <engin.dogusoy@lifemote.com>
2025-04-18 13:34:19 +0300

    updated permissions for kb

--- terraform/modules/knowledge_base/iam.tf
+++ terraform/modules/knowledge_base/iam.tf
@@ -1,3 +1,41 @@
+data "aws_caller_identity" "this" {}
+data "aws_partition" "this" {}
+data "aws_region" "this" {}
+
+
+locals {
+  account_id            = data.aws_caller_identity.this.account_id
+  partition             = data.aws_partition.this.partition
+  region                = data.aws_region.this.name  
+}
+
+resource "aws_iam_role" "boardgamebot_knowledge_base" {
+  name               = "bedrock-kb-role"
+  assume_role_policy = data.aws_iam_policy_document.boardgamebot_knowledge_base_assume_role.json
+}
+
+data "aws_iam_policy_document" "boardgamebot_knowledge_base_assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["bedrock.amazonaws.com"]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "aws:SourceAccount"
+      values   = [local.account_id]
+    }
+    condition {
+      test     = "ArnLike"
+      variable = "aws:SourceArn"
+      values   = ["arn:${local.partition}:bedrock:${local.region}:${local.account_id}:knowledge-base/*"]
+    }
+  }
+}
+
+
+
 resource "aws_opensearchserverless_access_policy" "boardgamebot_kb_aoss_policy" {
   name = "bgb-kb-aoss-access-policy"
   type = "data"
@@ -76,20 +114,7 @@ resource "aws_opensearchserverless_security_policy" "boardgamebot_kb_network_pol
   ])
 }
 
-resource "aws_iam_role" "boardgamebot_knowledge_base" {
-  name               = "bedrock-kb-role"
-  assume_role_policy = data.aws_iam_policy_document.boardgamebot_knowledge_base_assume_role.json
-}
 
-data "aws_iam_policy_document" "boardgamebot_knowledge_base_assume_role" {
-  statement {
-    actions = ["sts:AssumeRole"]
-    principals {
-      type        = "Service"
-      identifiers = ["bedrock.amazonaws.com"]
-    }
-  }
-}
 
 resource "aws_iam_role_policy" "boardgamebot_kb_s3_policy" {
   name = "boardgamebot-knowledge-base-s3-policy"
